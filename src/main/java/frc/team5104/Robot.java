a2ab262af80b70fcd6017998af734e1c2d73610c
mchow101 <mintychocolate101@gmail.com>
2020-01-11 17:34:30 -0800

    Update Robot.java

--- src/main/java/frc/team5104/Robot.java
+++ src/main/java/frc/team5104/Robot.java
@@ -2,35 +2,39 @@
 package frc.team5104;
 
 import frc.team5104.util.setup.RobotController;
-import edu.wpi.first.wpilinj.smartdashboard.SmartDashboard;
-import edu.wpi.first.networktables.NetWorkTable;
-import edu.wpi.first.networktables.NetWorkTableEntry;
-import edu.wpi.first.netowerktables.NetwrokTableInstance;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Robot extends RobotController.BreakerRobot {
+	NetworkTable table;
+	NetworkTableEntry tx;
+	NetworkTableEntry ty;
+	NetworkTableEntry ta;
+	
 	public Robot() {
-		NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-		NetworkTableEntry tx = table.getEntry("tx");
-		NetworkTableEntry ty = table.getEntry("ty");
-		NetworkTableEntry ta = table.getEntry("ta");
 		
-		double x = tx.getDouble(0.0);
-		double y = ty.getDouble(0.0);
-		double area = ta.getDouble(0.0);
-		
-		SmartDashboard.putNumber("LimelightX", x);
-		SmartDashboard.putNumber("LimelightY", y);
-		SmartDashboard.putNumber("LimelightArea", area);
 	}
 
 	public void teleopStart() {
-		
+		table = NetworkTableInstance.getDefault().getTable("limelight");
+		tx = table.getEntry("tx");
+		ty = table.getEntry("ty");
+		ta = table.getEntry("ta");
 	}
 	public void teleopStop() {
 		
 	}
 	public void teleopLoop() {
 		
+		
+		double x = tx.getDouble(0.0);
+		double y = ty.getDouble(0.0);
+		double area = ta.getDouble(0.0);
+		
+		System.out.println("LimelightX: " + x);
+		System.out.println("LimelightY: " + y);
+		System.out.println("LimelightArea: " + area);
 	}
 	
 	public void testLoop() {
